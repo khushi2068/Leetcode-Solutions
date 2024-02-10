1d59313950e2781ab37661a5a155a55b63394417
Khushi Agarwal <khushiagarwal2068@gmail.com>
2024-02-10 17:41:58 +0530

    Time: 160 ms, Memory: 88.6 MB - LeetHub

---
+++ contains-duplicate-iii/contains-duplicate-iii.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) 
+    {
+           vector<pair<int,int>> arr(nums.size());
+           for(int i=0;i<nums.size();i++)
+           {
+               arr[i].first=nums[i];
+               arr[i].second=i;
+           }
+           sort(arr.begin(),arr.end());
+           for(int i=0;i<nums.size();i++)
+           {
+               for(int j=i+1;j<nums.size();j++)
+               {
+                   if(arr[i].first+valueDiff>=arr[j].first)
+                   {
+                       if(abs(arr[i].second-arr[j].second)<=indexDiff)
+                            return true;
+                   }
+                   else
+                        break;
+               }
+           }
+           return false;
+    }
+};
