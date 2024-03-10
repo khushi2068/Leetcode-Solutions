547a04d7779b829fb7f4c61c71d437fb715cb85b
Khushi Agarwal <khushiagarwal2068@gmail.com>
2024-03-10 19:22:48 +0530

    Time: 22 ms, Memory: 31.9 MB - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    TreeNode* findRightmost(TreeNode* root) {
-        while (root->right != NULL) {
+    TreeNode* findrightmost(TreeNode* root){
+        while(root->right != NULL){
             root = root->right;
         }
         return root;
     }
-
+    
+    TreeNode* helper(TreeNode* root){
+        if (root->left == NULL) return root->right;
+        if (root->right == NULL) return root->left;
+        
+        TreeNode *left = root->left;
+        TreeNode *rightmost = findrightmost(left);
+        rightmost->right = root->right;
+        return left;
+    }
+    
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (root == NULL) return root;
-        if (root->val == key) {
-            // Node with only one child or no child
-            if (root->left == NULL) {
-                return root->right;
-            } else if (root->right == NULL) {
-                return root->left;
-            }
-            // Node with two children: Get the inorder predecessor (rightmost in left subtree)
-            TreeNode* inOrderPredecessor = findRightmost(root->left);
-            // Copy the inOrderPredecessor's content to root
-            root->val = inOrderPredecessor->val;
-            // Delete the inOrderPredecessor
-            root->left = deleteNode(root->left, inOrderPredecessor->val);
-        } else if (root->val > key) {
-            root->left = deleteNode(root->left, key);
-        } else {
-            root->right = deleteNode(root->right, key);
-        }
+        if (root->val == key) return helper(root);
+        if (root->val > key) root->left = deleteNode(root->left, key);
+        else root->right = deleteNode(root->right, key);
         return root;
     }
 };
