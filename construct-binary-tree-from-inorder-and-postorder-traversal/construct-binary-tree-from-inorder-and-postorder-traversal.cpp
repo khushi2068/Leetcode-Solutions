714c3989de7514611e4da909fa223bda27d8d9ad
Khushi Agarwal <khushiagarwal2068@gmail.com>
2024-11-14 18:37:34 +0530

    Time: 1 ms, Memory: 27.6 MB - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     
-    TreeNode* helper(vector<int>& inorder,int is, int ie, vector<int>& postorder, int ps, int pe) {
+    TreeNode* helper(vector<int>& inorder,int is, int ie, vector<int>& postorder, int ps, int pe, unordered_map<int,int> &m) {
         if((is>ie) ||( ps>pe)) return NULL;
         TreeNode* root=new TreeNode(postorder[pe]);
-        int idx=find(inorder.begin(),inorder.end(),root->val)-inorder.begin();
+        int idx=m[root->val];
         
-        root->left=helper(inorder,is,idx-1,postorder,ps,ps+idx-is-1);
-        root->right=helper(inorder,idx+1,ie,postorder,ps+idx-is,pe-1);
+        root->left=helper(inorder,is,idx-1,postorder,ps,ps+idx-is-1,m);
+        root->right=helper(inorder,idx+1,ie,postorder,ps+idx-is,pe-1,m);
         return root;
         
         
@@ -27,11 +27,16 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         
         if(postorder.empty()) return NULL;
+        
+        unordered_map<int,int> m;
+        for(int i=0;i<inorder.size();i++){
+            m[inorder[i]]=i;
+        }
         int is=0;
         int ie=inorder.size()-1;
         int ps=0;
         int pe=postorder.size()-1;
-        return helper(inorder,is,ie,postorder,ps,pe);
+        return helper(inorder,is,ie,postorder,ps,pe,m);
         
     }
 };
